bcccb23c98d9a18c02d1614e8a3b768f43b83acb
luisolimpio <lipeolimpio12@gmail.com>
2021-02-17 21:07:55 -0300

    Change folders structure

--- src/middlewares/auth.js
+++
@@ -1,9 +0,0 @@
-const jwt = require('jsonwebtoken');
-
-module.exports = (request, response, next) => {
-  const authHeader = request.headers.authorization;
-
-  if (!authHeader) return response.status(401).json({ message: 'Token is missing' })
-
-  return next();
-}
